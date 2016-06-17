af3e411e85aa2e5177adc05b03e372d18a2b6379
maglu <magnus.lucchese@gmail.com>
2016-06-16 23:15:28 -0700

    first functional version

--- max31865.c
+++ max31865.c
@@ -1,17 +1,15 @@
-/*
- * Baseado na biblioteca "Arduino driver library for the MAX31865."
- * Copyright (C) 2015 Ole Wolf <wolf@blazingangles.com>
+/**************************************************************************
+ * C driver library for the MAX31865.
  *
+ * Work Copyright 2016 Magnus Lucchese <magnus.lucchese@gmail.com>
  *
- * Wire the circuit as follows, assuming that level converters have been
- * added for the 3.3V signals:
- *
- *    Arduino Uno            -->  MAX31865
- *    ------------------------------------
- *    CS: any available pin  -->  CS
- *    MOSI: pin 11           -->  SDI (mandatory for hardware SPI)
- *    MISO: pin 12           -->  SDO (mandatory for hardware SPI)
- *    SCK: pin 13            -->  SCLK (mandatory for hardware SPI)
+ * This driver is based on Ole Wolf <wolf@blazingangles.com> arduino driver
+ * 
+ * The user should define the macros below
+ * CS_ENABLE(cs_pin) 
+ * CS_DISABLE(cs_pin)
+ * SPI_WRITE(value)  
+ * SPI_READ()
  *
  *
  * This program is free software: you can redistribute it and/or modify
@@ -26,211 +24,159 @@
  *
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
- */ 
-#include <asf.h>
-#include <stdint.h>
+**************************************************************************/
 #include <math.h>
 #include "max31865/max31865.h"
-#include "temperature/temperature.h"
-#include "rtc_ptc_spi/rtc_ptc_spi.h"
 
-void reconfigure(Max31865Control *ptc);
-double resistance(Max31865Control *ptc);
-double temperature(Max31865Control *ptc);
-uint8_t read_all(Max31865Control *ptc );
-void wr_spi( uint8_t data);
-uint8_t rd_spi(void);
-void max318655_configure( PtcId id, bool v_bias, bool conversion_mode, bool one_shot,
-                           bool three_wire, uint8_t fault_cycle, bool fault_clear,
-                           bool filter_50hz, uint16_t low_threshold,
-                           uint16_t high_threshold );
-Max31865Control ptc_control[PTC_LAST] = {
-   { .cs_pin = PTC_1_CS_OUTPUT_PIN, .type = RTD_PT100 },
-   { .cs_pin = PTC_2_CS_OUTPUT_PIN, .type = RTD_PT100 },
-   { .cs_pin = PTC_3_CS_OUTPUT_PIN, .type = RTD_PT100 },
-   { .cs_pin = PTC_4_CS_OUTPUT_PIN, .type = RTD_PT100 },
-   { .cs_pin = PTC_5_CS_OUTPUT_PIN, .type = RTD_PT100 },
-   { .cs_pin = PTC_6_CS_OUTPUT_PIN, .type = RTD_PT100 },
-};
-
-Status Max31865GetTemperature(Temperature *temp, PtcId id, bool *failed) {
-   Max31865Control *ptc;
-   ptc = &ptc_control[id];
-   max318655_configure(id, true, false,true,true, 0x00, true, false,0,0);
-   vTaskDelay(10);
-   read_all(ptc);
-   *failed = false;
-   *temp = temperature(ptc);
-   return SUCCESS;
-}
+void configure_device(struct Max31865Device *instance);
+void set_threshold_values(struct Max31865Device *instance, uint16_t config_low_threshold, uint16_t config_high_threshold );
+float calculate_temperature(float resistance, float resistance_0);
 
 /**
- * Configure the MAX31865.  The parameters correspond to Table 2 in the MAX31865
- * datasheet.  The parameters are combined into a control bit-field that is stored
- * internally in the class for later reconfiguration, as are the fault threshold values.
+ * The initializer for the MAX31865 device 
  *
- * @param [in] v_bias Vbias enabled (@a true) or disabled (@a false).
- * @param [in] conversion_mode Conversion mode auto (@a true) or off (@a false).
- * @param [in] one_shot 1-shot measurement enabled (@a true) or disabled (@a false).
- * @param [in] three_wire 3-wire enabled (@a true) or 2-wire/4-wire (@a false).
- * @param [in] fault_detection Fault detection cycle control (see Table 3 in the MAX31865
- *             datasheet).
- * @param [in] fault_clear Fault status auto-clear (@a true) or manual clear (@a false).
- * @param [in] filter_50hz 50 Hz filter enabled (@a true) or 60 Hz filter enabled
- *             (@a false).
- * @param [in] low_threshold Low fault threshold.
- * @param [in] high_threshold High fault threshold.
-*/
-void max318655_configure( PtcId id, bool v_bias, bool conversion_mode, bool one_shot,
-                          bool three_wire, uint8_t fault_cycle, bool fault_clear,
-                          bool filter_50hz, uint16_t low_threshold,
-                          uint16_t high_threshold ) {
-  
-  uint8_t control_bits = 0;
+ * @param [in] instance device instance
+ * @param [in] config configuration for device
+ */
+void max31865_init(struct Max31865Device *instance,struct Max31865Config *config) {
+    /* CS pin for the SPI device. */
+    instance->cs_pin = config->cs_pin;
 
-  /* Assemble the control bit mask. */
-  control_bits |= ( v_bias ? 0x80 : 0 );
-  control_bits |= ( conversion_mode ? 0x40 : 0 );
-  control_bits |= ( one_shot ? 0x20 : 0 );
-  control_bits |= ( three_wire ? 0x10 : 0 );
-  control_bits |= fault_cycle & 0b00001100;
-  control_bits |= ( fault_clear ? 0x02 : 0 );
-  control_bits |= ( filter_50hz ? 0x01 : 0 );
+    /* Set the confgiuration register */
+    uint8_t control_bits = 0;
+    control_bits |= ( config->conversion_mode ? 0x40 : 0 );
+    control_bits |= ( config->one_shot ? 0x20 : 0 );
+    control_bits |= ( config->three_wire ? 0x10 : 0 );
+    control_bits |= config->fault_cycle & 0b00001100;
+    control_bits |= ( config->filter_50hz ? 0x01 : 0 );
+    instance->config_control_bits = control_bits;
 
-  /* Store the control bits and the fault threshold limits for reconfiguration
-     purposes. */
-  Max31865Control *ptc = &ptc_control[id];
-  ptc->configuration_control_bits   = control_bits;
-  ptc->configuration_low_threshold  = low_threshold;
-  ptc->configuration_high_threshold = high_threshold;
+    /* Set the PTD. */
+    instance->resistance_at_zero = config->resistance_at_zero;
+    instance->resistance_reference = config->resistance_reference;
 
-  /* Perform an initial "reconfiguration." */
-  reconfigure(ptc);
+    /* Send configuration to device */
+    configure_device(instance);
+
+    /* Send threshold to device */
+    set_threshold_values(instance, config->low_threshold, config->high_threshold);
 }
 
 /**
- * Reconfigure the MAX31865 by writing the stored control bits and the stored fault
- * threshold values back to the chip.
- */ 
-void reconfigure(Max31865Control *ptc) {
-   GetRtcSpiPort();
-
-   /* Write the configuration to the MAX31865. */
-   SelectRtcSpi( ptc->cs_pin, true );
-   wr_spi( 0x80 );
-   wr_spi( ptc->configuration_control_bits );
-   SelectRtcSpi( ptc->cs_pin, false );
-
-
-   /* Write the threshold values. */
-   SelectRtcSpi( ptc->cs_pin, true );
-   
-   wr_spi( 0x83 );
-   wr_spi( ( ptc->configuration_high_threshold >> 8 ) & 0x00ff );
-   wr_spi(   ptc->configuration_high_threshold        & 0x00ff );
-   wr_spi( ( ptc->configuration_low_threshold >> 8 )  & 0x00ff );
-   wr_spi(   ptc->configuration_low_threshold         & 0x00ff );
-   SelectRtcSpi( ptc->cs_pin, false );
-   ReleaseRtcSpiPort();
+ * Enable/Disable Vbias for MAX31865
+ *
+ * @param [in] instance device instance
+ * @param [in] enable true, turn on vbias, false, turn off vbias
+ */
+void max31865_set_vbias(struct Max31865Device *instance, bool enable) {
+    if(enable) {
+        SETBIT(instance->config_control_bits, 7);
+    } else {
+        CLEARBIT(instance->config_control_bits, 7);
+    }
+    configure_device(instance);
 }
 
-double resistance(Max31865Control *ptc) {
-   double rtd_rref = ( ptc->type == RTD_PT100 ) ? (double)RTD_RREF_PT100 : (double)RTD_RREF_PT1000;
-   return( (double)ptc->measured_resistance * rtd_rref / (double)RTD_ADC_RESOLUTION );
+/**
+ * Get default config values for MAX31865 device 
+ *
+ * @param [in] config configuration for device
+ */
+void max31865_get_config_default(struct Max31865Config *config) {
+    config->cs_pin = 0;
+    config->resistance_at_zero = 100;
+    config->resistance_reference = 400;
+    config->conversion_mode = true;
+    config->one_shot = false;
+    config->three_wire = false;
+    config->fault_cycle = 0;
+    config->filter_50hz = false;      
+    config->low_threshold = 0;
+    config->high_threshold = 32767;
+}
+
+/**
+ * Get temperature value in oC for device
+ *
+ * @param [in] instance device instance
+ * @param [out] temperature measured temperature
+ * @return 0 if OK, fault register value if error
+ */
+uint8_t max31865_get_temperature(struct Max31865Device *instance, float *temperature) {
+    uint16_t read_reg;
+    float resistance;
+    uint8_t fault_register = 0;
+
+    /* Read resistance measured value */
+    CS_ENABLE( instance->cs_pin);
+    SPI_WRITE( REG_RTD_MSB );
+    read_reg  = SPI_READ() << 8;
+    read_reg |= SPI_READ();
+    CS_DISABLE( instance->cs_pin);
+
+    /* Check for fault */
+    if ( TESTBIT(read_reg,0) ) {
+        /* Fault: read fault register */
+        CS_ENABLE( instance->cs_pin);
+        SPI_WRITE( REG_FAULT_STATUS );
+        fault_register = SPI_READ();
+        CS_DISABLE( instance->cs_pin);
+
+        /*Clear fault register */ 
+        instance->config_control_bits |= 0x02;
+        configure_device(instance);
+        return fault_register;
+    }
+    read_reg = read_reg >> 1;
+
+    resistance = (read_reg * instance->resistance_reference) / (1u << 15);
+    *temperature = calculate_temperature(resistance, instance->resistance_at_zero);
+    return 0;
 }
 
 /**
  * Apply the Callendar-Van Dusen equation to convert the RTD resistance
  * to temperature:
- *
- *   \f[
- *   t=\frac{-A\pm \sqrt{A^2-4B\left(1-\frac{R_t}{R_0}\right)}}{2B}
- *   \f],
- *
- * where
- *
- * \f$A\f$ and \f$B\f$ are the RTD coefficients, \f$R_t\f$ is the current
- * resistance of the RTD, and \f$R_0\f$ is the resistance of the RTD at 0
- * degrees Celsius.
- *
- * For more information on measuring with an RTD, see:
- * <http://newton.ex.ac.uk/teaching/CDHW/Sensors/an046.pdf>.
- *
- * @param [in] resistance The measured RTD resistance.
- * @return Temperature in degrees Celsius.
+ * Tr = (-A + SQRT(delta) ) / 2*B
+ * delta = A^2 - 4B*(1-Rt/Ro)
+ * @param [in] resistance measured resistance
+ * @param [in] resistance_0 constant resistance at 0oC
+ * @return calculated temperature
  */
-double temperature(Max31865Control *ptc) {
-  static const double a2   = 2.0 * RTD_B;
-  static const double b_sq = RTD_A * RTD_A;
-
-  const double rtd_resistance =
-    ( ptc->type == RTD_PT100 ) ? RTD_RESISTANCE_PT100 : RTD_RESISTANCE_PT1000;
-
-  double c = 1.0 - resistance(ptc) / rtd_resistance;
-  double D = b_sq - 2.0 * a2 * c;
-  double temperature_deg_C = ( -RTD_A + sqrt( D ) ) / a2;
-
-  return( temperature_deg_C );
+float calculate_temperature(float resistance, float resistance_0) {
+    float temperature;
+    float delta = (RTD_A * RTD_A) - 4 * RTD_B * (1.0 - resistance/resistance_0);
+    temperature = (-RTD_A + sqrt(delta)) / (2 * RTD_B);
+    return temperature;
 }
 
 /**
- * Read all settings and measurements from the MAX31865 and store them
- * internally in the class.
+ * Set device configuration register
  *
- * @return Fault status byte
+ * @param [in] instance device instance
  */
-uint8_t read_all(Max31865Control *ptc ) {
-   uint16_t combined_bytes;
-   GetRtcSpiPort();
-  
-   /* Start the read operation. */
-   SelectRtcSpi( ptc->cs_pin, true );
-  
-   /* Tell the MAX31865 that we want to read, starting at register 0. */
-   wr_spi( 0x00 );
-
-   /* Read the MAX31865 registers in the following order:
-         Configuration
-         RTD
-         High Fault Threshold
-         Low Fault Threshold
-         Fault Status */
-   ptc->measured_configuration = rd_spi();
-  
-  
-   combined_bytes  = rd_spi() << 8;
-   combined_bytes |= rd_spi();
-   ptc->measured_resistance = combined_bytes >> 1;
-   
-   combined_bytes  = rd_spi() << 8;
-   combined_bytes |= rd_spi();
-   ptc->measured_high_threshold = combined_bytes >> 1;
-
-   combined_bytes  = rd_spi() << 8;
-   combined_bytes |= rd_spi();
-   ptc->measured_low_threshold = combined_bytes >> 1;
-
-   ptc->measured_status = rd_spi();
-
-   SelectRtcSpi( ptc->cs_pin, false );
-   ReleaseRtcSpiPort();
-
-   /* Reset the configuration if the measured resistance is
-      zero or a fault occurred. */
-   if(    ( ptc->measured_resistance == 0 )
-      || ( ptc->measured_status != 0 ) ) {
-      reconfigure( ptc );
-   }
-
-   return ptc->measured_status;
+void configure_device(struct Max31865Device *instance) {
+  CS_ENABLE( instance->cs_pin);
+  SPI_WRITE( WR(REG_CONFIG) );
+  SPI_WRITE( instance->config_control_bits );
+  CS_DISABLE( instance->cs_pin );
 }
 
-void wr_spi( uint8_t data) {
-   WriteRtcPtcSpi(&data, 1);
+/**
+ * Set device fail threshold registers
+ *
+ * @param [in] instance device instance
+ * @param [in] config_low_threshold threshold for fail on low value
+ * @param [in] config_high_threshold threshold for fail on high value
+ */
+void set_threshold_values(struct Max31865Device *instance, uint16_t config_low_threshold, uint16_t config_high_threshold ) {
+  CS_ENABLE( instance->cs_pin);
+  SPI_WRITE( WR(REG_HIGH_FAULT_THR_MSB) );
+  SPI_WRITE( ( config_high_threshold >> 8 ) & 0x00ff );
+  SPI_WRITE(   config_high_threshold        & 0x00ff );
+  SPI_WRITE( ( config_low_threshold >> 8 ) & 0x00ff );
+  SPI_WRITE(   config_low_threshold        & 0x00ff );
+  CS_DISABLE( instance->cs_pin );
 }
 
-uint8_t rd_spi(void) {
-   uint8_t value_read;
-   ReadRtcPtcSpi(&value_read, 1);
-   return value_read;
-}
+
--- max31865.h
+++ max31865.h
@@ -1,18 +1,51 @@
-/*
- * max31865.h
+/**************************************************************************
+ * C driver library for the MAX31865.
  *
- * Created: 1/21/2016 1:52:14 PM
- *  Author: Magnus
- */ 
+ * Work Copyright 2016 Magnus Lucchese <magnus.lucchese@gmail.com>
+ *
+ * This driver is based on Ole Wolf <wolf@blazingangles.com> arduino driver
+ * 
+ * The user should define the macros below
+ * CS_ENABLE(cs_pin) 
+ * CS_DISABLE(cs_pin)
+ * SPI_WRITE(value)  
+ * SPI_READ()
+ *
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+**************************************************************************/
+#ifndef _MAX31865_H
+#define _MAX31865_H
 
+#include <stdint.h>
+#include <stdbool.h>
 
-#ifndef MAX31865_H_
-#define MAX31865_H_
-#include "util/util.h"
-#include "indrel/indrel_data_types.h"
-#include "temperature/temperature.h"
+#warning "Re-define the macros below acording to your hardware"
+#include <asf.h>
+extern struct spi_module spi_master_instance;
 
-Status Max31865GetTemperature(Temperature *temperature, PtcId id, bool *failed);
+static inline uint8_t my_spi_read_(void) {
+   uint16_t rx_data = 0;
+   spi_read(&spi_master_instance, &rx_data);
+   return (uint8_t) rx_data;
+}
+
+#define CS_ENABLE(cs_pin)        do { port_pin_set_output_level(cs_pin, false); } while (0) /**< Chip select enable macro  */
+#define CS_DISABLE(cs_pin)       do { port_pin_set_output_level(cs_pin, true); } while (0)  /**< Chip select disable macro */
+#define SPI_WRITE(value)         do { spi_write(&spi_master_instance, value); } while (0)   /**< SPI Write one byte macro  */
+#define SPI_READ()               my_spi_read_()                                             /**< SPI Read one byte macro   */
+
 
 #define MAX31865_FAULT_HIGH_THRESHOLD  ( 1 << 7 )
 #define MAX31865_FAULT_LOW_THRESHOLD   ( 1 << 6 )
@@ -26,96 +59,71 @@ Status Max31865GetTemperature(Temperature *temperature, PtcId id, bool *failed);
 #define MAX31865_FAULT_DETECTION_MANUAL_1  ( 0x02 << 2 )
 #define MAX31865_FAULT_DETECTION_MANUAL_2  ( 0x03 << 2 )
 
-
+/* Read Register Address */
+#define REG_CONFIG                  0x00
+#define REG_RTD_MSB                 0x01
+#define REG_RTD_LSB                 0x02
+#define REG_HIGH_FAULT_THR_MSB      0x03
+#define REG_HIGH_FAULT_THR_LSB      0x04
+#define REG_LOW_FAULT_THR_MSB       0x05
+#define REG_LOW_FAULT_THR_LSB       0x06
+#define REG_FAULT_STATUS            0x07
+#define WR(reg)                     ( (reg) | 0x80 )
 
 /* RTD data, RTD current, and measurement reference
    voltage. The ITS-90 standard is used; other RTDs
    may have coefficients defined by the DIN 43760 or
    the U.S. Industrial (American) standard. */
+
 #define RTD_A_ITS90         3.9080e-3
 #define RTD_A_USINDUSTRIAL  3.9692e-3
 #define RTD_A_DIN43760      3.9848e-3
 #define RTD_B_ITS90         -5.870e-7
 #define RTD_B_USINDUSTRIAL  -5.8495e-7
 #define RTD_B_DIN43760      -5.8019e-7
+
 /* RTD coefficient C is required only for temperatures
    below 0 deg. C.  The selected RTD coefficient set
    is specified below. */
-#define SELECT_RTD_HELPER(x) x
-#define SELECT_RTD(x) SELECT_RTD_HELPER(x)
-#define RTD_A         SELECT_RTD(RTD_A_ITS90)
-#define RTD_B         SELECT_RTD(RTD_B_ITS90)
-/*
- * The reference resistor on the hardware; see the MAX31865 datasheet
- * for details.  The values 400 and 4000 Ohm are recommended values for
- * the PT100 and PT1000.
+//#define SELECT_RTD_HELPER(x) x
+//#define SELECT_RTD(x) SELECT_RTD_HELPER(x)
+#define RTD_A         (RTD_A_ITS90)
+#define RTD_B         (RTD_B_ITS90)
+
+/**
+ * Device instance control struct
  */
-#define RTD_RREF_PT100         400 /* Ohm */
-#define RTD_RREF_PT1000       4000 /* Ohm */
-/*
- * The RTD resistance at 0 degrees Celcius.  For the PT100, this is 100 Ohm;
- * for the PT1000, it is 1000 Ohm.
- */
-#define RTD_RESISTANCE_PT100   100 /* Ohm */
-#define RTD_RESISTANCE_PT1000 1000 /* Ohm */
-
-#define RTD_ADC_RESOLUTION  ( 1u << 15 ) /* 15 bits */
-typedef enum ptd_type { RTD_PT100, RTD_PT1000 };
-   
-#if 0
-/* See the main (MAX31865.cpp) file for documentation of the class methods. */
-class MAX31865_RTD
-{
-public:
-  
-
-  MAX31865_RTD( ptd_type type, uint8_t cs_pin );
-  void configure( bool v_bias, bool conversion_mode, bool one_shot, bool three_wire,
-                  uint8_t fault_cycle, bool fault_clear, bool filter_50hz,
-                  uint16_t low_threshold, uint16_t high_threshold );
-  uint8_t read_all( );
-  double temperature( ) const;
-  uint8_t status( ) const { return( measured_status ); }
-  uint16_t low_threshold( ) const { return( measured_low_threshold ); }
-  uint16_t high_threshold( ) const  { return( measured_high_threshold ); }
-  uint16_t raw_resistance( ) const { return( measured_resistance ); }
-  double resistance( ) const
-  {
-    const double rtd_rref =
-      ( this->type == RTD_PT100 ) ? (double)RTD_RREF_PT100 : (double)RTD_RREF_PT1000;
-    return( (double)raw_resistance( ) * rtd_rref / (double)RTD_ADC_RESOLUTION );
-  }
-
-private:
-  /* Our configuration. */
+struct Max31865Device {
   uint8_t  cs_pin;
-  ptd_type type;
-  uint8_t  configuration_control_bits;
-  uint16_t configuration_low_threshold;
-  uint16_t configuration_high_threshold;
-  void reconfigure( );
-
-  /* Values read from the device. */
-  uint8_t  measured_configuration;
-  uint16_t measured_resistance;
-  uint16_t measured_high_threshold;
-  uint16_t measured_low_threshold;
-  uint8_t  measured_status;
+  uint8_t  config_control_bits;
+  uint16_t resistance_at_zero;
+  uint16_t resistance_reference;
 };
-#endif
-typedef struct {
-   uint8_t  cs_pin;
-   enum ptd_type type;
-   uint8_t  configuration_control_bits;
-   uint16_t configuration_low_threshold;
-   uint16_t configuration_high_threshold;
 
-   /* Values read from the device. */
-   uint8_t  measured_configuration;
-   uint16_t measured_resistance;
-   uint16_t measured_high_threshold;
-   uint16_t measured_low_threshold;
-   uint8_t  measured_status;
-}Max31865Control;
+/**
+ * Configuration struct to the MAX31865.
+ */
+struct Max31865Config {
+    uint8_t  cs_pin;                /**< Chip select pin */
+    uint16_t resistance_at_zero;    /**< Sensor resistance at 0 Celsius (100 for PT100, 1000 for PT1000). */
+    uint16_t resistance_reference;  /**< Circuit reference resistance (recommended on MAX31865 datasheet: 400 ohms for PT100, 4000 for PT1000).*/
+    bool conversion_mode;           /**< Conversion_mode Conversion mode auto (@a true) or off (@a false). */
+    bool one_shot;                  /**< One_shot 1-shot measurement enabled (@a true) or disabled (@a false). */
+    bool three_wire;                /**< Three_wire 3-wire enabled (@a true) or 2-wire/4-wire (@a false). */
+    uint8_t fault_cycle;            /**< Fault_detection Fault detection cycle control (see Table 3 in the MAX31865 datasheet). */
+    bool filter_50hz;               /**< Filter_50hz 50 Hz filter enabled (@a true) or 60 Hz filter enabled (@a false). */
+    uint16_t low_threshold;         /**< Low fault threshold. */
+    uint16_t high_threshold;        /**< High fault threshold. */
+};
 
-#endif /* MAX31865_H_ */
+/* Bit manipulation macros */
+#define BIT(pos)           (1 << (pos))
+#define SETBIT(x,y)        (x) |= BIT(y)
+#define CLEARBIT(x,y)      (x) &= ~BIT(y)
+#define TESTBIT(data,pos)  ((0u == (data & BIT(pos)))?0u:1u)
+
+void max31865_init(struct Max31865Device *instance,struct Max31865Config *config);
+void max31865_set_vbias(struct Max31865Device *instance, bool enable);
+uint8_t max31865_get_temperature(struct Max31865Device *instance, float *temperature);
+void max31865_get_config_default(struct Max31865Config *config);
+#endif /* _MAX31865_H */
